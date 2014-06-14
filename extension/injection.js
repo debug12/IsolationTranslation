6d5b01227137f2a33b2de239aec9075f64f58cd7
Eric Song <eric.song@rutgers.edu>
2014-06-14 11:01:43 -0400

    made matches bold

--- extension/injection.js
+++ extension/injection.js
@@ -1,28 +1,22 @@
 console.log("hello from inject.js");
 
 chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-    var state = response.state;
-    console.log("State: " + state);
-
-    var regexp_str = "";
-
-    for(var property in grammar_words){
-      if(grammar_words.hasOwnProperty(property)){
-	regexp_str += property + "|";
-      }
-    }
-    regexp_str = regexp_str.substring(0, regexp_str.length-1);
-
-    console.log(regexp_str);
+    var regexp_str = response.regexp;
+    var dict = response.dict; 
 
     $(document).ready(function(){
       var count = 0;
 
       findAndReplaceDOMText(document, {
 	find: new RegExp(regexp_str, "g"), 
+	wrap: 'strong'
+      });
+
+      findAndReplaceDOMText(document, {
+	find: new RegExp(regexp_str, "g"), 
 	replace: function(portion, match) {
 	  count++;
-	  return grammar_words[match[0]][0];
+	  return dict[match[0]][0];
 	}
       });
 
