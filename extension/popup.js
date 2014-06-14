d4c55376b4e230f14deab2bed26cc74baba2fe82
debug12 <kumarde@umich.edu>
2014-06-14 06:28:17 -0400

    Added basic buttons for view

---
+++ extension/popup.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+	var UnGrammarNazi = document.getElementById("UnGrammarNazi");
+	var GrammarNazi = document.getElementById("GrammarNazi");
+
+	UnGrammarNazi.onclick = function(){
+		//Does some stuff
+	}
+
+	GrammarNazi.onclick = function(){
+		//does some other stuff
+	}
+
+
+}
