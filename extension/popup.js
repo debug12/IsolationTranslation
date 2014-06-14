97aebe9b9b08792741093c774383433891794559
debug12 <kumarde@umich.edu>
2014-06-14 06:37:23 -0400

    added 2 more buttons for functionality

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,8 @@
 window.onload = function(){
 	var UnGrammarNazi = document.getElementById("UnGrammarNazi");
 	var GrammarNazi = document.getElementById("GrammarNazi");
+	var Racist = document.getElementById("Racist");
+	var Stupid = document.getElementById("Stupid");
 
 	UnGrammarNazi.onclick = function(){
 	        console.log("hi #1");
@@ -11,6 +13,15 @@ window.onload = function(){
 	        console.log("hi #2");
 		//does some other stuff
 	}
+
+	Racist.onclick = function(){
+		console.log("hi #3");
+	}
+
+	Stupid.onclick = function(){
+		console.log("hi #4");
+	}
+
 
 
 }
