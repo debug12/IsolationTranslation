f90402e7f6981ace7166dd9fec857bcabc0a6853
debug12 <kumarde@umich.edu>
2014-06-14 04:45:39 -0400

    added mega-object key value pair

--- GrammarUnNazi/words.js
+++ GrammarUnNazi/words.js
@@ -11,23 +11,35 @@ request("http://www.kimonolabs.com/api/c8w6goti?apikey=e476f7f5eb03cb4204125c51c
 
 var parseWords = function(wordsArray){
 	var cleanedWordsArray = [];
+	var wordObj = {};
 	for(var i = 0; i < wordsArray.length; ++i){
 		var stringToParse = wordsArray[i].Words.text;
 		var arraySplitBySpaces = stringToParse.split(" ");
+		var correctlySpelledWordKey = "";
 
-		var wordObj = {
-			correctlySpelledWord: "",
-			incorrectlySpelledWord : []
-		};
-		wordObj.correctlySpelledWord = arraySplitBySpaces[0];
-		
-		var isPartOfExplanation = false;
+		if(arraySplitBySpaces[1] != '–'){
+			correctlySpelledWordKey = arraySplitBySpaces[0] + " " + arraySplitBySpaces[1];
+			//wordObj.correctlySpelledWord = arraySplitBySpaces[0] + " " + arraySplitBySpaces[1];	
+		}
+		else {
+			if(arraySplitBySpaces[0].indexOf('/') !== -1){
+				correctlySpelledWordKey = arraySplitBySpaces[0].split('/')[0];
+			}else{
+				correctlySpelledWordKey = arraySplitBySpaces[0];
+			}
+			//wordObj.correctlySpelledWord = arraySplitBySpaces[0]; //this is the properly spelled word
+		}
+		var isPartOfExplanation = false; //bool to hackCheck
+		var incorrectWordArray = [];
 		for(var j = 2; j < arraySplitBySpaces.length; ++j){
 			var incorrectWordToParse = arraySplitBySpaces[j];
 			
 			incorrectWordToParse = incorrectWordToParse.replace(/,/g, ''); //removes commas
 			incorrectWordToParse = incorrectWordToParse.replace(/\[.*?\]/g, ''); //removes entries within square brackets
-
+			/*
+				What on earth!? You might think the following is a load of garbage code, but trust me,
+				it wouldn't be this awful if wikipedia normalized their data a little bit. Ugh.
+			*/
 			if(incorrectWordToParse.indexOf('(') !== -1){
 				isPartOfExplanation = true;
 				continue;
@@ -42,13 +54,14 @@ var parseWords = function(wordsArray){
 			if(incorrectWordToParse === '') {
 				continue;
 			}
-			if(incorrectWordToParse === '-') {
+			if(incorrectWordToParse == '–') {
 				continue;
 			}
-			incorrectWordToParse = incorrectWordToParse.replace(/[()]/g, '');
-			wordObj.incorrectlySpelledWord.push(incorrectWordToParse);
+
+			incorrectWordToParse = incorrectWordToParse.replace(/[()]/g, ''); //Still removes some parens that weren't caught initially
+			incorrectWordArray.push(incorrectWordToParse);
 		}
-		cleanedWordsArray.push(wordObj);
+		wordObj[correctlySpelledWordKey] = incorrectWordArray;
 	}
-	return JSON.stringify(cleanedWordsArray);
+	return JSON.stringify(wordObj);
 }
