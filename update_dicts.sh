90c2aca4ae8e7e850790b0bc7d66dc209d3f4bad
debug12 <kumarde@umich.edu>
2014-06-14 11:01:11 -0400

    added the most offensive commit ever

--- update_dicts.sh
+++ update_dicts.sh
@@ -5,3 +5,6 @@ node ./GrammarUnNazi/words.js >> ./extension/grammar.js
 
 echo 'var reverse_words = ' > ./extension/reverse.js
 node ./GrammarNazi/words.js >> ./extension/reverse.js
+
+echo 'var racial_words = ' > ./extension/racial.js
+node ./Racial/slur.js >> ./extension/racial.js
